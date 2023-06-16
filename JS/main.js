d9850bf40cb1ad38c81cd0f3719d9601c2ef02e6
TaeHyun <kt9811@naver.com>
2023-06-16 12:50:14 +0900

    포트폴리오

--- JS/main.js
+++ JS/main.js
@@ -50,30 +50,6 @@ setTimeout(typing, 1500);
 
 
 
-// function countingType1(value, num) {
-//   if (num == 0) {
-//     value.innerHTML = "0";
-//   } else {
-//     /* 입력한 숫자를 33번에 걸쳐 0부터 올림. */
-//     const each = Math.ceil(num / 90);
-//     let time = 0;
-
-//     for (let i = 0; i <= num; i += each) {
-//       setTimeout(() => {
-//         value.innerHTML = i + "%";
-//       }, 20 * time);
-//       /* 딱 떨어지지 않는 숫자를 마지막에 그 숫자로 만들어주기 위함 */
-//       if (i + each > this.maxNum1) {
-//         setTimeout(() => {
-//           value.innerHTML = num;
-//         }, 20 * (time + 1));
-//       }
-//       time++;
-//     }
-//   }
-// }
-
-
 //헤더 스크롤
 const header = document.querySelector(".header");
 
@@ -190,7 +166,7 @@ gsap.from(percent, 2, {
   textContent: 0, //시작숫자
   scrollTrigger: {
     trigger: skill,
-    start: 'top 40%',
+    start: 'top 30%',
   },
   stagger: {
     onUpdate: function () {
@@ -227,7 +203,6 @@ const projectSwiper = new Swiper(".swiper", {
     el: ".swiper-pagination",
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
